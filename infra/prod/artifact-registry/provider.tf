a8d877c438c4d5a9ada0dee4cd10f1d07ca79b6f
Yash-Patel02 <yash.patel@symphonize.com>
2025-10-14 16:56:23 +0530

    chnaged the credentials path

--- infra/prod/artifact-registry/provider.tf
+++ infra/prod/artifact-registry/provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
   region      = var.REGION
   project     = var.PROJECT_ID
-  credentials = file("C:/Users/persimmon-ai-cff4da30503c 2.json")
+  credentials = file("${HOME}/gcp-key.json")
 }
 
 provider "google-beta" {
-  credentials = file("C:/Users/persimmon-ai-cff4da30503c 2.json")
+  credentials = file("${HOME}/gcp-key.json")
   region      = var.REGION
   project     = var.PROJECT_ID
 }
