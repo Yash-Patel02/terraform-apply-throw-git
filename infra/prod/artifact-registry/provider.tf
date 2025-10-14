5f74edf96674d3447e90888ca4b218cd16ca94c9
Yash-Patel02 <yash.patel@symphonize.com>
2025-10-14 17:02:15 +0530

    changed workflow

--- infra/prod/artifact-registry/provider.tf
+++ infra/prod/artifact-registry/provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
   region      = var.REGION
   project     = var.PROJECT_ID
-  credentials = file("${HOME}/gcp-key.json")
+  credentials = file("/home/runner/gcp-key.json")
 }
 
 provider "google-beta" {
-  credentials = file("${HOME}/gcp-key.json")
+  credentials = file("/home/runner/gcp-key.json")
   region      = var.REGION
   project     = var.PROJECT_ID
 }
