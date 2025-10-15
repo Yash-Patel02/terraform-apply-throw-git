a96b5102e60ec3d28db5b2d11fdc179130a56577
Mukeswar <vasanamukeswar@gmail.com>
2025-10-15 15:16:23 +0530

    checking

--- infra/prod/artifact-registry/backend.tf
+++ infra/prod/artifact-registry/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "akiam-statefiles-storage-bucket"
     prefix = "aikam-tf-qa/artifact-registry"
-    credentials = "home/runner/gcp-key.json"
+    credentials = file("/home/runner/gcp-key.json")
   }
 }
