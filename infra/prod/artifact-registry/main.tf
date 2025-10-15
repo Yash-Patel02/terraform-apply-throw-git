85c14da88ce19aea53abb3240958672968980e6c
Yash-Patel02 <yash.patel@symphonize.com>
2025-10-15 10:10:08 +0530

    removed n8n

--- infra/prod/artifact-registry/main.tf
+++ infra/prod/artifact-registry/main.tf
@@ -15,11 +15,3 @@ module "frontend_artifact_registry" {
   repository_id = "${var.REPOSITORY_ID}-frontend"
   image_name    = var.APP_IMAGE_NAME
 }
-module "n8n_artifact_registry" {
-  source = "../../../modules/artifact-registry"
-
-  project_id    = var.PROJECT_ID
-  region        = var.REGION_CONTAINER_REPO
-  repository_id = var.REPOSITORY_ID
-  image_name    = var.N8N_IMAGE_NAME
-}
