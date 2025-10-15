f9334ee82df05f4f196c8b62ad1933fc5094286c
Mukeswar <vasanamukeswar@gmail.com>
2025-10-15 15:52:42 +0530

    checking

--- infra/prod/frontend-service/backend.tf
+++ infra/prod/frontend-service/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "gcs" {
-    bucket = "akiam-statefiles-storage-bucket"
-    prefix = "aikam-tf-qa/frontend-service"
-    credentials = "home/runner/gcp-key.json"
-  }
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "akiam-statefiles-storage-bucket"
+#     prefix = "aikam-tf-qa/frontend-service"
+#     credentials = "home/runner/gcp-key.json"
+#   }
+# }
